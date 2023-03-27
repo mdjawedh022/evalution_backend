6958d1032b0022d409c5873f58475c99ed91650d
Md Jawed <mdjawedh22@gmail.com>
2023-03-27 16:37:52 +0530

    done

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,18 +1,19 @@
 const jwt=require("jsonwebtoken");
 
 const auth=(req,res,next)=>{
-    const token=req.headers.authorization
-    if(token){
-        const decoded=jwt.verify(token,"link")
-        if(decoded){
-            req.body.userID=decoded.userID
-            next();
-        } else{
-            res.status(400).send({"msg":"Please login 1st"})
-        } 
-    } else{
-        res.status(400).send({"msg":"Please login 1st!"})
-    }
+  const token=req.headers.authorization.split(" ")[1]
+
+if(token){
+  const decoded=jwt.verify(token,"masai")
+  if(decoded){
+    req.body.userID=decoded.userID
+    next();
+  } else{
+    res.status(400).send({'msg':"plz login first!"})
+  }
+} else{
+    res.status(400).send({"msg":"please login first"})
+}
 }
 
 module.exports={auth}
